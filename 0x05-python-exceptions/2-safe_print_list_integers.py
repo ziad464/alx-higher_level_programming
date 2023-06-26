2d8540f324c1fe7553fb899a733ac5c2671e66e4
ziad464 <zezozeyad7654@gmail.com>
2023-06-25 23:29:11 -0700

    update

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/user/bin/Python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """print the first x elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    reet = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i], end="")
+                    ret += 1
+                    except (ValueError, TypeError):
+                    continue
+                print("")
+                return (ret)
