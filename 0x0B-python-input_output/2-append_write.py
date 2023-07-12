aa7106b51f4114018cdb8991df70f17302644f07
ziad464 <zezozeyad7654@gmail.com>
2023-07-12 10:14:07 -0700

    update

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,17 @@
-!/usr/bin/python3
-is_same_class = __import__('2-is_same_class').is_same_class
+#!/usr/bin/python3
+"""Defines a class-checking function."""
 
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
 
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of a given class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    if type(obj) == a_class:
+        return True
+    return False
