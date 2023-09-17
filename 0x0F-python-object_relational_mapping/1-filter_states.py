6c0f00ca59992336d05e124eb6799112d7851869
ziad464 <zezozeyad7654@gmail.com>
2023-09-17 10:39:15 -0700

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
+"""
+List all states starting with 'N' from a MySQL db on localhost at port 3306
+"""
+
+from mysqlman import MySQLMan
+from MySQLdb import Error
+from sys import argv, exit, stderr
 
 
-import MySQLdb
-from sys import argv
+HELP = '{} username password database'.format(argv[0])
+HOST = 'localhost'
+PORT = 3306
 
-'''
-lists all states with starting name with N
-from the database hbtn_0e_0_usa
-'''
-if __name__ == "__main__":
-    con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = con.cursor()
-    cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY 'N%'ORDER BY id ASC")
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
+
+if __name__ == '__main__':
+    try:
+        mysqlman = MySQLMan(
+            connect=True,
+            user=argv[1],
+            password=argv[2],
+            database=argv[3],
+            host=HOST,
+            port=PORT,
+        )
+    except IndexError:
+        stderr.write('usage: {}\n'.format(HELP))
+        exit(2)
+    except Error as e:
+        stderr.write('{}\n'.format(e.args[1]))
+        exit(1)
+    query = "SELECT id, name FROM states ORDER BY id;"
+    results = mysqlman.query([query, ()])
+    for row in results[0]:
+        if row[1].startswith('N'):
+            print(row)
